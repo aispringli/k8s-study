18727adcfdf6a5064864830ad05e985b2d4d4373
aispring <3377939225@qq.com>
2021-06-17 21:23:22 +0800

    resource cors

--- aispring/aispring-https/deploy-https.sh
+++ aispring/aispring-https/deploy-https.sh
@@ -1,5 +1,5 @@
 kubectl -n aispring create secret generic aispring-https-certs --from-file=tls.key --from-file=tls.crt
 
-kubectl -n aispring create secret generic aispring-https-certs --from-file=tls.key --from-file=tls.crt --dry-run -o yaml | kubectl apply -f -
+kubectl -n aispring create secret generic aispring-https-certs --from-file=tls.key --from-file=tls.crt  --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl -n aispring get secret aispring-https-certs -o yaml
