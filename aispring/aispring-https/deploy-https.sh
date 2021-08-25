f8702b5e3e47bbbc4bb79ab2c18a582fde16cce3
li.chunying <li.chunying@xinheyun.com>
2021-08-25 18:31:40 +0800

    web local

--- aispring/aispring-https/deploy-https.sh
+++ aispring/aispring-https/deploy-https.sh
@@ -1,5 +1,3 @@
-kubectl -n aispring create secret generic aispring-https-certs --from-file=tls.key --from-file=tls.crt
+kubectl -n aispring create secret generic local-aispring-https-certs --from-file=tls.key --from-file=tls.crt  --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl -n aispring create secret generic aispring-https-certs --from-file=tls.key --from-file=tls.crt  --dry-run=client -o yaml | kubectl apply -f -
-
-kubectl -n aispring get secret aispring-https-certs -o yaml
+kubectl -n aispring get secret local-aispring-https-certs -o yaml
